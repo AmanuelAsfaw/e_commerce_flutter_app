25cd01b26f829ffff18aebc42361de43f1f33a19
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-08-09 15:58:47 +0300

    Initialize firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,36 @@
 import 'package:ecommerce_app/screens/login.dart';
-import 'package:ecommerce_app/screens/signup.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+bool shouldUseFirebaseEmulator = false;
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  // if (!kIsWeb) {
+  //   await Firebase.initializeApp();
+  // } else {
+  //   try {
+  //     await Firebase.initializeApp(
+  //       options: const FirebaseOptions(
+  //         apiKey: 'AIzaSyC5aNieKhi9XYZdnUsrgtojtGOIg71dzp8',
+  //         appId: '1:631463811506:android:c7435547d780075e202cab',
+  //         messagingSenderId: '448618578101',
+  //         projectId: 'ecommerce-flutter-app-e8efa',
+  //         authDomain: 'ecommerce-flutter-app-e8efa.firebaseapp.com',
+  //         databaseURL: 'https://ecommerce-flutter-app-e8efa.firebaseio.com',
+  //         storageBucket: 'ecommerce-flutter-app-e8efa.appspot.com',
+  //         measurementId: 'G-F79DJ0VFGS',
+  //       ),
+  //     );
+  //   } on FirebaseException catch (e) {
+  //     print(e.message);
+  //   }
+  // }
+
   runApp(const MyApp());
 }
 
